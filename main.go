5f62bd0eafd027d33a6881db89521755600fc0bd
Rudycode <46867584+Rudycode@users.noreply.github.com>
2019-05-14 16:17:59 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func indexHandler(writer http.ResponseWriter, request *http.Request)  {
+	template, err := template.ParseFiles("templates/index.html", "templates/head.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprint(writer, err.Error())
+	}
+
+	template.ExecuteTemplate(writer, "index", nil)
+}
+
+func loginHandler(writer http.ResponseWriter, request *http.Request)  {
+	template, err := template.ParseFiles("templates/login.html", "templates/head.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprint(writer, err.Error())
+	}
+
+	template.ExecuteTemplate(writer, "login", nil)
+}
+
+func main() {
+	fmt.Println("Listening Port 8080")
+
+	//
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
