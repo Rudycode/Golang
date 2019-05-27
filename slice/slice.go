057583f045012808ee9e1917c607cff94f7aaa84
Rudycode <46867584+Rudycode@users.noreply.github.com>
2019-05-27 13:47:58 +0300

    Update slice.go

--- slice/slice.go
+++ slice/slice.go
@@ -6,10 +6,10 @@ import (
 
 func Pic(dx, dy int) [][]uint8 {
 	a := make([][]uint8, dy)
-	for x, _ := range a {
-		a[x] = make([]uint8, dx)
-		for y, _ := range a[x] {
-			a[x][y] = uint8(x^y)
+	for y, _ := range a {
+		a[y] = make([]uint8, dx)
+		for x, _ := range a[y] {
+			a[y][x] = uint8(x^y)
 		}
 	}
 	return a
