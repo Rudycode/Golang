917b37c1a03b2dcbd31d0126287e3bccb96c5de7
Rudycode <46867584+Rudycode@users.noreply.github.com>
2019-06-19 01:08:10 +0300

    Update main.go

--- passgenfunc/main.go
+++ passgenfunc/main.go
@@ -6,30 +6,41 @@ import (
 	"time"
 )
 
-const DefaultString = "0123456789"
+/*
+a function with closure, in which a map with unique keys is generated, with given a length. 
+If the key/value already exists during generation, the generated key is increased by an order 
+and this is repeated until the map is ready, the length specified.
+*/
+
+const DefaultString = "0123456789" // constant where we will take a random number
+type emptyStruct struct{}
+
+// generate random integer
 func randInt(min int, max int) int {
 	return min + rand.Intn(max-min)
 }
+
+// returns the function() map[string]string where a unique key-value of length "X" was generated
 func generationPass(x int) func() map[string]string {
 	generatedLen := x
 	generatedMap := make(map[string]string, generatedLen)
-	return func() map[string]string {
+	return func() map[string]string{
 		min := 0
 		max := len(DefaultString)
-		var m string
-		var sum int
-		for {
-			m = string(DefaultString[randInt(min, max)])
-			for i := 0; i < sum; i++ {
-				m += string(DefaultString[randInt(min, max)])
+		var resultValue string
+		var degree int
+		 for {
+			resultValue = string(DefaultString[randInt(min, max)])
+			for i := 0; i < degree; i++ {
+				resultValue += string(DefaultString[randInt(min, max)])
 			}
-			_, ok := generatedMap[m]
+			_, ok := generatedMap[resultValue]
 			if ok {
-				sum++
+				degree++
 			}
-			generatedMap[m] = m
+			generatedMap[resultValue] = resultValue
 
-			if len(generatedMap) >= (generatedLen - 1) {
+			if len(generatedMap) >= generatedLen {
 				break
 			}
 		}
@@ -38,8 +49,10 @@ func generationPass(x int) func() map[string]string {
 }
 
 func main() {
+	t := time.Now()
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	first := generationPass(300)
-	fmt.Println("In Main1", first(), len(first()))
+	 firstMap := generationPass(300)
+	fmt.Println("In Main1", len(firstMap()))
+	fmt.Println(time.Since(t))
 }
